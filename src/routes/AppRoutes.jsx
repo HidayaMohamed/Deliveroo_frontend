109c88ff239e9ead8c3c5a98f45584f201af189e
Hidaya Mohamed <hidayamohamed002@gmail.com>
2026-02-02 17:37:20 +0300

    Update AppRoute to include the PublicRoute

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,45 @@
 import { Routes, Route } from "react-router-dom";
+import Landing from "../pages/Landing";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Unauthorized from "../pages/Unauthorized";
 import ProtectedRoute from "../components/ProtectedRoute";
+import PublicRoute from "../components/PublicRoute";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      {/* Public-only pages */}
+      <Route
+        path="/"
+        element={
+          <PublicRoute>
+            <Landing />
+          </PublicRoute>
+        }
+      />
+
+      <Route
+        path="/login"
+        element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        }
+      />
+
+      <Route
+        path="/register"
+        element={
+          <PublicRoute>
+            <Register />
+          </PublicRoute>
+        }
+      />
+
       <Route path="/unauthorized" element={<Unauthorized />} />
 
+      {/* Protected pages */}
       <Route
         path="/user"
         element={
