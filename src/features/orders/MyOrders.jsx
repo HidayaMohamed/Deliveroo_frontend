a0b1f571510a914522c5105db19a8d82c3f4f215
Sharon Njoroge <waithirasharon222@gmail>
2026-02-02 19:52:04 +0300

    Add customer orders list page

--- src/features/orders/MyOrders.jsx
+++ src/features/orders/MyOrders.jsx
@@ -1,1 +1,21 @@
+import { useEffect, useState } from "react";
+import { getMyOrders } from "./ordersAPI";
+import OrderCard from "../../components/orders/OrderCard";
+
+export default function MyOrders() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    getMyOrders().then((res) => setOrders(res.data));
+  }, []);
+
+  return (
+    <div>
+      <h2>My Orders</h2>
+      {orders.map((order) => (
+        <OrderCard key={order.id} order={order} />
+      ))}
+    </div>
+  );
+}
 
