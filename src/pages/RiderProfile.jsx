a3f007e6ebac5494da3b57759d6327e653283220
Hidaya Mohamed <hidayamohamed002@gmail.com>
2026-02-12 09:29:21 +0300

    Fix import error

--- src/pages/RiderProfile.jsx
+++ src/pages/RiderProfile.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
-import { logOut }
 import {
   User,
   Phone,
